8658a42745839576a9c8857cce2f2c931c8d0df3
AbhiPatil5 <101649647+AbhiPatil5@users.noreply.github.com>
2025-01-13 10:09:06 +0100

    add

---
+++ asppythonlearning/Firstapp/loop.py
@@ -0,0 +1,23 @@
+for i in range (10):
+    print("My Python-",i)
+
+
+
+colors=["red","blue","black","pink","white"]
+
+for x in colors:
+    print(x)
+print("---------------------")
+
+colors.append("brown")
+
+for x in colors:
+    print(x)
+
+print("-------while loop--------------")
+
+while(i<10):
+    print ("Print-",i)
+    i=i+1
+
+
---
+++ asppythonlearning/Firstapp/lsittuple.py
@@ -0,0 +1,13 @@
+mylist=["rg1","rg2","rg3","rg4","rg5"]
+
+print (mylist)
+
+mytuple=("rg1","rg2","rg3","rg4","rg5")
+
+print(mytuple)
+
+mylist.append("rg6")
+
+mylist.remove("rg4")
+
+print (mylist)
